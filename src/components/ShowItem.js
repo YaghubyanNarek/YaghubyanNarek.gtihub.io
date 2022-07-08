efb762c1569140311d2921699db632e510c83863
YaghubyanNarek <yaghubyann05@gmail.com>
2022-07-08 16:24:04 +0400

    created close icon for pop-up

--- src/components/ShowItem.js
+++ src/components/ShowItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
 export default function ShowItem(props) {
   return (
@@ -9,6 +10,12 @@ export default function ShowItem(props) {
       <h2>{props.mainItem.title}</h2>
       <p>{props.mainItem.desc}</p>
       <b>{props.mainItem.price}$</b>
+      <FaTimes 
+        className='mute-item'
+        onClick={() =>{
+          props.showFullItem(props.mainItem)
+        }}
+        />
       <div className='add-to-cart' onClick={() => props.onAdd(props.mainItem)}>+</div>
     </div>
   )
